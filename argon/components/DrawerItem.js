7bf3b9951ce14e1ea627379539a9979429dc5899
osmosishk <tonyho@osmosis.com.hk>
2021-10-14 16:43:51 +0800

    14102021

---
+++ argon/components/DrawerItem.js
@@ -0,0 +1,137 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Linking } from "react-native";
+import { Block, Text, theme } from "galio-framework";
+
+import Icon from "./Icon";
+import argonTheme from "../constants/Theme";
+
+class DrawerItem extends React.Component {
+  renderIcon = () => {
+    const { title, focused } = this.props;
+
+    switch (title) {
+      case "Home":
+        return (
+          <Icon
+            name="shop"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : argonTheme.COLORS.PRIMARY}
+          />
+        );
+      case "Follow Up":
+        return (
+          <Icon
+            name="map-big"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : argonTheme.COLORS.ERROR}
+          />
+        );
+      case "Events":
+        return (
+          <Icon
+            name="spaceship"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : argonTheme.COLORS.PRIMARY}
+          />
+        );
+      case "Energy Profile":
+        return (
+          <Icon
+            name="chart-pie-35"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : argonTheme.COLORS.WARNING}
+          />
+        );
+      case "Dashboard":
+        return (
+          <Icon
+            name="calendar-date"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : argonTheme.COLORS.INFO}
+          />
+        );
+      case "Control":
+        return (
+          <Icon
+            name="calendar-date"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : argonTheme.COLORS.DEFAULT}
+          />
+        );
+      case "Getting Started":
+        return (
+          <Icon
+            name="spaceship"
+            family="ArgonExtra"
+            size={14}
+            color={focused ? "white" : "rgba(0,0,0,0.5)"}
+          />
+        );
+      case "Log out":
+        return <Icon />;
+      default:
+        return null;
+    }
+  };
+
+  render() {
+    const { focused, title, navigation } = this.props;
+
+    const containerStyles = [
+      styles.defaultStyle,
+      focused ? [styles.activeStyle, styles.shadow] : null
+    ];
+
+    return (
+      <TouchableOpacity
+        style={{ height: 60 }}
+        onPress={() => title == 'Guide' ? Linking.openURL('https://system.innovura.app').catch((err) => console.error('An error occurred', err)) : navigation.navigate(title)}
+      >
+        <Block flex row style={containerStyles}>
+          <Block middle flex={0.1} style={{ marginRight: 5 }}>
+            {this.renderIcon()}
+          </Block>
+          <Block row center flex={0.9}>
+            <Text
+              style={{ fontFamily: "open-sans-regular" }}
+              size={15}
+              bold={focused ? true : false}
+              color={focused ? "white" : "rgba(0,0,0,0.5)"}
+            >
+              {title}
+            </Text>
+          </Block>
+        </Block>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  defaultStyle: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    marginBottom: 2
+  },
+  activeStyle: {
+    backgroundColor: argonTheme.COLORS.ACTIVE,
+    borderRadius: 4
+  },
+  shadow: {
+    shadowColor: theme.COLORS.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1
+  }
+});
+
+export default DrawerItem;
